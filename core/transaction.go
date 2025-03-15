d18b9d27e7747d8cb873778736ac9904d868bbb8
谷朝雨 <1191745821@qq.com>
2025-03-15 17:52:32 +0800

    platform can sync by PBFT for once

---
+++ core/transaction.go
@@ -0,0 +1,71 @@
+package core
+
+import (
+	"fmt"
+
+	"github.com/anthdm/projectx/crypto"
+	"github.com/anthdm/projectx/types"
+)
+
+type Transaction struct {
+	Data      []byte
+	From      crypto.PublicKey
+	Signature *crypto.Signature
+
+	// cached version of the tx data hash
+	hash       types.Hash
+	firstSceen int64
+}
+
+func (tx *Transaction) SetFirstSceen(t int64) {
+	tx.firstSceen = t
+}
+
+func (tx *Transaction) FirstSceen() int64 {
+	return tx.firstSceen
+}
+
+func NewTransaction(data []byte) *Transaction {
+	return &Transaction{
+		Data: data,
+	}
+}
+
+func (tx *Transaction) Hash(hasher Hasher[*Transaction]) types.Hash {
+	if tx.hash.IsZero() {
+		tx.hash = hasher.Hash(tx)
+	}
+	return tx.hash
+}
+
+func (tx *Transaction) Sign(privKey crypto.PrivateKey) error {
+	sig, err := privKey.Sign(tx.Data)
+	if err != nil {
+		return err
+	}
+
+	tx.From = privKey.PublicKey()
+	tx.Signature = sig
+
+	return nil
+}
+
+func (tx *Transaction) Verify() error {
+	if tx.Signature == nil {
+		return fmt.Errorf("transaction has no signature")
+	}
+
+	if !tx.Signature.Verify(tx.From, tx.Data) {
+		return fmt.Errorf("invalid transaction signature")
+	}
+
+	return nil
+}
+
+func (tx *Transaction) Decode(dec Decoder[*Transaction]) error {
+	return dec.Decode(tx)
+}
+
+func (tx *Transaction) Encode(enc Encoder[*Transaction]) error {
+	return enc.Encode(tx)
+}
